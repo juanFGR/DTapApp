f7411b4a66ec46df313ec6b59eccacb5d0d35331
juan_gonzalez <juan.ramos.fg1@gmail.com>
2014-05-06 13:45:08 +0000

    Preparando despliegue

--- platforms/android/assets/www/js/dtapapp-parse.js
+++ platforms/android/assets/www/js/dtapapp-parse.js
@@ -2,6 +2,10 @@
 			Parse.initialize("e9x1rMT71C6CNbd2nIeY5RauWTznaexHmdaUePvc", "1NLogz7TbWfxZssM8K04hXNh1XO1dfQjCkUv5wzO");
 		}
 			
+		function setBar(key,value){
+			localStorage.setItem(key, value);
+		}	
+		
 		function printList(query, rootElement, printProperty, onClickFunction) {
 			query.find({
 				success: function(results) {
@@ -9,20 +13,29 @@
 						var object = results[i];
 						onClickString = '';
 						if(onClickFunction) {
+													
 							onClickString = 'onClick='+onClickFunction+'("'+object.id+'")';
 						}
 						if(onClickFunction ==='printTapas' ){
-						rootElement.append('<div class="secondMenu" onClickString>'+
-       '<div class="colorPrimary listLocal" >'+object.get(printProperty)+'</div>'+
-        '<a href="../pages/mapa_u.html"><img src="../img/secondMenuB.JPG" width="50" height="50"></a>'+
-        '<a href="../pages/tapas.html"><img src="../img/secondMenuC.JPG" width="50" height="50"></a>'+
+						
+						rootElement.append('<div class="secondMenu">'+
+       '<div  class="colorPrimary listLocal" >'+object.get(printProperty)+'</div>'+
+        '<a class="iconImage" href="../pages/mapa_g.html"><img src="../img/secondMenuB.JPG" width="50" height="50"></a>'+
+        '<a class="iconImage" onclick="setBar('+"'barID'"+','+"'"+object.id+"'"+')" href="../pages/tapas.html"><img src="../img/secondMenuC.JPG" width="50" height="50"></a>'+
         '</div>');
 
         }else if(onClickFunction ==='printRestaurants'){
-			localStorage.setItem("myValue", object.id);
-       rootElement.append('<a href="../pages/locales.html"><div class="colorPrimary listLocal" >'+object.get(printProperty)+'</div></a>');
+			localStorage.setItem("rutaID", object.id);
+       rootElement.append('<div onclick="javascript:window.location.href=\'../pages/locales.html\'" class="colorPrimary listRutas" >'+object.get(printProperty)+'</div>');
 
-        }
+        }else{
+			alert(object.id);
+		rootElement.append('<div><div class="colorPrimary listTapas" ><div style="float:left;">'+
+		'<img src="../img/secondMenuB.JPG" width="50" height="50"></div><div>'+object.get(printProperty)+'</div>'+
+		'</div><a href="../pages/mapa_u.html"><img src="../img/secondMenuB.JPG" width="50" height="50"></a>'+
+		'<a  href="../pages/tapas.html"><img src="../img/secondMenuC.JPG" width="50" height="50"> </a></div>');
+			
+			}
 					    }
 				},
 				error: function(error) {
@@ -48,7 +61,22 @@
 			printList(findTapaByRestaurantQuery(restaurantId), $("#"+rootElement), 'name');
 		}
 		
-		
+		function loadRestaurants(query,rootElement) {
+			query.find({
+				success: function(results) {
+					for (var i = 0; i < results.length; i++) {
+						var object = results[i];
+						var Latlng = new google.maps.LatLng(object.get('location').latitude,object.get('location').longitude);
+					
+					var marker = new google.maps.Marker({position: Latlng,map: rootElement,title: object.get('name')});
+									
+					    }
+				},
+				error: function(error) {
+
+				}
+			});
+		}
 	
 //--------------------------
 //--------------------------
